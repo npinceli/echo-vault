9be60eb8f0094e3e8787090c892f9cb45d7695ce
npinceli <pincelinicolas@gmail.com>
2024-07-28 14:38:30 -0300

    configs iniciais, login e cadastro

---
+++ backend/routes.py
@@ -0,0 +1,38 @@
+from app import app, db
+from flask import request, jsonify
+from models import User
+from passlib.hash import bcrypt
+
+
+# Criar usuario
+@app.route("/register", methods=['POST'])
+def register():
+    data = request.json
+    username = data['username']
+    password = bcrypt.hash(data['password'])
+
+    # Verifica se ja tem um usuario com o mesmo nome
+    if User.query.filter_by(username=username).first():
+        return jsonify({'error', 'Usuário já cadastrado!'}), 400
+
+    new_user = User(username=username, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'success', 'Seja bem-vindo!'}), 200
+
+
+# Realizar o login
+@app.route("/login", methods=['POST'])
+def login():
+    data = request.json
+    username = data['username']
+    password = data['password']
+
+    user = User.query.filter_by(username=username).first()
+
+    # Verifica se tem usuario e se a senha é a mesma.
+    if user and bcrypt.verify(password, user.password):
+        return jsonify({'success', 'Bem-vindo de volta!'}), 200
+
+    return jsonify({'error', 'Dados incorretos!'}), 400
