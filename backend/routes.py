ac5c04dacb82272cfd7bedf538ab70c2ffd489d2
npinceli <pincelinicolas@gmail.com>
2024-07-28 15:23:18 -0300

    corrigindo rotas e requisicoes

--- backend/routes.py
+++ backend/routes.py
@@ -5,34 +5,34 @@ from passlib.hash import bcrypt
 
 
 # Criar usuario
-@app.route("/register", methods=['POST'])
+@app.route('/api/register', methods=["POST"])
 def register():
     data = request.json
-    username = data['username']
-    password = bcrypt.hash(data['password'])
+    username = data.get("username")
+    password = bcrypt.hash(data.get("password"))
 
     # Verifica se ja tem um usuario com o mesmo nome
     if User.query.filter_by(username=username).first():
-        return jsonify({'error', 'Usuário já cadastrado!'}), 400
+        return jsonify({'error': 'Usuário já cadastrado!'}), 400
 
     new_user = User(username=username, password=password)
     db.session.add(new_user)
     db.session.commit()
 
-    return jsonify({'success', 'Seja bem-vindo!'}), 200
+    return jsonify({'success': 'Seja bem-vindo!'}), 201
 
 
 # Realizar o login
-@app.route("/login", methods=['POST'])
+@app.route('/api/login', methods=['POST'])
 def login():
     data = request.json
-    username = data['username']
-    password = data['password']
+    username = data.get("username")
+    password = data.get("password")
 
     user = User.query.filter_by(username=username).first()
 
     # Verifica se tem usuario e se a senha é a mesma.
     if user and bcrypt.verify(password, user.password):
-        return jsonify({'success', 'Bem-vindo de volta!'}), 200
+        return jsonify({'success': 'Bem-vindo de volta!'}), 200
 
-    return jsonify({'error', 'Dados incorretos!'}), 400
+    return jsonify({'error': 'Dados incorretos!'}), 400
